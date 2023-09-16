c155bc6f7b2d325fd67308b7cc8279ced4ff7c17
marcoyimyaem <marcoyimyaem@gmail.com>
2023-09-16 13:40:52 +0800

    added scanner

--- src/main/java/org/example/ArrayListExample.java
+++ src/main/java/org/example/ArrayListExample.java
@@ -1,7 +1,6 @@
 package org.example;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class ArrayListExample {
     public static void main(String[] args) {
@@ -19,8 +18,22 @@ public class ArrayListExample {
         System.out.println(ulam.add("Egg"));
         System.out.println(ulam.remove("Adobo"));
         System.out.println(ulam);
-
-
-
+        Double num1 = Double.valueOf(56.5);
+        System.out.println(num1.intValue());
+        Integer s1 = 180;
+        Integer s2 = 180;
+        System.out.println(s1+s2);
+        List<String>list = new ArrayList<>();
+        list.add("Robin");
+        list.add("Hawk");
+        Object[] objectArray = list.toArray();
+        String[] stringArray= list.toArray(new String[0]);
+        Collections.sort(list);
+//        Collections.shuffle();
+        System.out.println(list);
+        System.out.println("enter a number");
+        Scanner input1 = new Scanner(System.in);
+        String message = ((input1.nextInt()%2) == 0) ? "Even" : "Odd";
+        System.out.println(message);
     }
 }
---
+++ src/main/java/org/example/JavaQuiz1.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class JavaQuiz1 {
+    public static void main(String[] args) {
+        boolean keepGoing = true;
+        int result = 15, i = 10;
+        do {
+            i--;
+            if(i==8) keepGoing = false;
+                result -= 2;
+            } while(keepGoing);
+        System.out.println(result);
+    }
+}
