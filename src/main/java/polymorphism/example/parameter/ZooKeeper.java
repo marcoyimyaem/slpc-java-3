037b47b3bf4c81f9afdfa654276dc0bdd847e29d
marcoyimyaem <marcoyimyaem@gmail.com>
2023-10-03 13:51:27 +0800

    Update ZooKeeper.java

--- src/main/java/polymorphism/example/parameter/ZooKeeper.java
+++ src/main/java/polymorphism/example/parameter/ZooKeeper.java
@@ -1,5 +1,5 @@
 package polymorphism.example.parameter;
-
+//added polymorphism
 public class ZooKeeper {
     public static void feed(Reptile reptile){
         System.out.println("Feeding reptile "+reptile.getName());
